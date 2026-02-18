3ea431baad4ce0e6e5e93d3c2abd1a121d15c232
rakib1248 <rakib25059159@gmail.com>
2026-02-18 14:37:04 +0300

    try cookie block problem solve

--- next.config.ts
+++ next.config.ts
@@ -1,35 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-
-  async headers() {
-    return [
-      {
-        // ব্যাকএন্ডের সাথে কুকি আদান-প্রদান সহজ করতে
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "https://foodhut-backend-k0z2.onrender.com",
-          },
-        ],
-      },
-    ];
-  },
-
   images: {
     domains: ["images.unsplash.com"],
   },
-  // async rewrites() {
-  //   return [
-  //     {
-  //       source: "/api/:path*",
-  //       destination: "https://foodhut-backend-k0z2.onrender.com/:path*",
-  //     },
-  //   ];
-  // },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://food-hub-back-end.vercel.app/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
