2eecd3b177e4a4f718b5794699e918ca5bca85b4
rakib1248 <rakib25059159@gmail.com>
2026-02-18 03:32:04 +0300

    try cookie block problem solve

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
   images: {
     domains: ["images.unsplash.com"],
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://foodhut-backend-k0z2.onrender.com/api/:path*", //
+      },
+    ];
   },
 };
 
