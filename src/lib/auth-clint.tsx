d6631e94b3d7819b7d050d1a7371bd4652a2da70
rakib1248 <rakib25059159@gmail.com>
2026-02-18 14:06:01 +0300

    try cookie block problem solve

--- src/lib/auth-clint.tsx
+++ src/lib/auth-clint.tsx
@@ -1,10 +1,9 @@
 import { env } from "@/env";
-import { createAuthClient } from "better-auth/react"; 
+import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  baseURL: env.NEXT_PUBLIC_AUTH_URL, //"http://localhost:5050/api/auth", 
+  baseURL: env.NEXT_PUBLIC_AUTH_URL, //"http://localhost:5050/api/auth",
   fetchOptions: {
-  
     credentials: "include",
   },
 });
